f67876adfd401f86e47297dedf89985049e0a508
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-03-03 09:24:16 -0300

    chore: seed.ts file adding 3 ticket types

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
 const prisma = new PrismaClient();
 
 async function main() {
@@ -7,16 +7,39 @@ async function main() {
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        endsAt: dayjs().add(21, 'days').toDate(),
       },
     });
   }
 
   console.log({ event });
+
+  await prisma.ticketType.createMany({
+    data: [
+      {
+        name: 'option1',
+        price: 250,
+        isRemote: false,
+        includesHotel: false,
+      },
+      {
+        name: 'option2',
+        price: 600,
+        isRemote: false,
+        includesHotel: true,
+      },
+      {
+        name: 'option3',
+        price: 100,
+        isRemote: true,
+        includesHotel: false,
+      },
+    ],
+  });
 }
 
 main()
