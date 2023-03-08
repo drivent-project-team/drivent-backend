bc7d8a05e2a42a5a91a5b328b85c27e8bf77fa06
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-03-08 09:50:59 -0300

    test: add getNumBookingsByRoom integration test

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -431,3 +431,60 @@ describe("PUT /booking", () => {
     });
   });
 });
+
+describe("GET /booking/count", () => {
+  it("should respond with status 401 if no token is given", async () => {
+    const response = await server.get("/booking");
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 200 when user has a booking ", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const payment = await createPayment(ticket.id, ticketType.price);
+
+      const hotel = await createHotel();
+      for(let i = 0; i < 10; i++) {
+        const room = await createRoomWithHotelId(hotel.id);
+        await createBooking({
+          userId: user.id,
+          roomId: room.id,
+        });
+      }
+
+      const response = await server.get("/booking/count").set("Authorization", `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.OK);
+      expect(response.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            roomId: expect.any(Number),
+            num_bookings: expect.any(Number),
+          })
+        ])
+      );
+    });
+  });
+});
