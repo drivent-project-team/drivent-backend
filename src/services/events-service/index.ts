8bf8266f4e834be04b997365ad3b227159f90e91
Brenda Rayane <brenda.rayane.0018@gmail.com>
2023-03-11 11:44:45 -0300

    feat: implementation of redis in the event route

--- src/services/events-service/index.ts
+++ src/services/events-service/index.ts
@@ -3,12 +3,25 @@ import eventRepository from "@/repositories/event-repository";
 import { exclude } from "@/utils/prisma-utils";
 import { Event } from "@prisma/client";
 import dayjs from "dayjs";
+import { createClient } from "redis";
 
 async function getFirstEvent(): Promise<GetFirstEventResult> {
-  const event = await eventRepository.findFirst();
-  if (!event) throw notFoundError();
+  const redis = createClient({
+    url: "redis://localhost:6379"
+  });
+  await redis.connect();
+  const cacheEvent = await redis.get("event");
+  console.log(cacheEvent);
 
-  return exclude(event, "createdAt", "updatedAt");
+  if (cacheEvent) {
+    return JSON.parse(cacheEvent);
+  } else {
+    const event = await eventRepository.findFirst();
+    if (!event) throw notFoundError();
+    redis.set("event", JSON.stringify(event));
+
+    return exclude(event, "createdAt", "updatedAt");
+  }
 }
 
 export type GetFirstEventResult = Omit<Event, "createdAt" | "updatedAt">;
