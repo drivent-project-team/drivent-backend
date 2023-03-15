ba158bcd0ae4bfea0540365f944ef4267aac965e
jepariz <je.parizotto@gmail.com>
2023-03-14 22:14:47 -0300

    fix: dates format

--- src/controllers/activities-controller.ts
+++ src/controllers/activities-controller.ts
@@ -2,16 +2,15 @@ import { AuthenticatedRequest } from '@/middlewares';
 import activitiesService from '@/services/activities-service';
 import { Response } from 'express';
 import httpStatus from 'http-status';
-
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
 
 export async function listActivities(req: AuthenticatedRequest, res: Response) {
-    try {
-      const dates = await activitiesService.getEventActivities();
-      return res.status(httpStatus.OK).send({
-        dates
-      });
-    } catch (error) {
-      return res.sendStatus(httpStatus.NOT_FOUND);
-    }
+  try {
+    const activities = await activitiesService.getEventActivities();
+    const eventDates = [...new Set(activities.map((activity) => dayjs(activity.date).locale('pt-br').format('dddd, D/MM')))];
+    return res.status(httpStatus.OK).send({ activities, eventDates });
+  } catch (error) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
   }
-  
+}
